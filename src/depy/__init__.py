b5d2631531f0a117b230a19e3af72e5d15f5703c
SebastianDHA <heinrich.sebastian.dohmhansen@gmail.com>
2025-10-17 13:10:42 +0100

    Bump version: 0.1.2 → 0.1.3

--- src/depy/__init__.py
+++ src/depy/__init__.py
@@ -4,7 +4,7 @@ from depy.summarized_py import SummarizedPy
 
 __author__ = """Sebastian Dohm-Hansen"""
 __email__ = 'heinrich.sebastian.dohmhansen@gmail.com'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 __all__ = [
     "column_selector",
     "summarized_py",
